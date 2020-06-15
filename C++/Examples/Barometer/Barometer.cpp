d683dca3ec4fe9afb22b3aaeb2f49dcd4431f4be
Mirko Wächter <mail@mirko-waechter.de>
2020-06-15 11:01:25 +0100

    Added average filter to barometer example

--- C++/Examples/Barometer/Barometer.cpp
+++ C++/Examples/Barometer/Barometer.cpp
@@ -13,6 +13,41 @@ make
 #include <Common/Util.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <boost/circular_buffer.hpp>
+#define C_TO_KELVIN 273.15f
+
+float get_altitude_difference(float base_pressure, float pressure, float temperature) 
+{
+    float ret;
+    float temp    = temperature + C_TO_KELVIN;
+    float scaling = pressure / base_pressure;
+
+    // This is an exact calculation that is within +-2.5m of the standard
+    // atmosphere tables in the troposphere (up to 11,000 m amsl).
+    ret = 153.8462f * temp * (1.0f - expf(0.190259f * logf(scaling)));
+
+    return ret;
+}
+
+class AverageFilter 
+{
+public:
+    AverageFilter(int size = 20) : buffer(size){
+
+    }
+    float update(float value)
+    {
+        buffer.push_back(value);
+        float sum = 0.0f;
+        for(auto v: buffer)
+        {
+            sum += v;
+        }
+        return sum/buffer.size();
+    }
+private:
+    boost::circular_buffer<float> buffer;
+};
 
 int main()
 {
@@ -23,7 +58,10 @@ int main()
     }
 
     barometer.initialize();
-
+    
+    AverageFilter filter, base_pressure_filter;
+    float base_pressure = -1;
+    long  count = 0;
     while (true) {
         barometer.refreshPressure();
         usleep(10000); // Waiting for pressure data ready
@@ -34,11 +72,18 @@ int main()
         barometer.readTemperature();
 
         barometer.calculatePressureAndTemperature();
-
-        printf("Temperature(C): %f Pressure(millibar): %f\n", 
-                barometer.getTemperature(), barometer.getPressure());
+        if(count < 100)
+        {
+            base_pressure = barometer.getPressure();
+            base_pressure = base_pressure_filter.update(base_pressure);
+        }
+        float relative_altitude = get_altitude_difference(base_pressure, barometer.getPressure(), barometer.getTemperature());
+        float filtered_relative_altitude = filter.update(relative_altitude);
+        printf("#%d: Temperature(C): %f Pressure(millibar): %f, relative height(m): %f, filtered: %f\n", count, 
+                barometer.getTemperature(), barometer.getPressure(), relative_altitude, filtered_relative_altitude);
                 
-        sleep(1);
+        //usleep(300000);
+        count ++;
     }
 
     return 0;
